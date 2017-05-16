16c5e03752fd0c963c6989ee5cad32b2485553e1
Michal Fousek <michal.fousek@showmax.com>
2017-05-16 12:19:11 +0200

    T22223 Add example project.

---
+++ Example/Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SHMAVPlayerInterfaceExampleTVOS
+//
+//  Created by Michal Fousek on 16/05/2017.
+//  Copyright © 2017 Showmax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
