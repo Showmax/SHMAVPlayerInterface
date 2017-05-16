90de1c242f0391f88565e8e465120bde4b23b99f
Michal Fousek <michal.fousek@showmax.com>
2017-05-16 12:27:50 +0200

    T22223 Add example player.

--- Example/Source/ViewController.swift
+++ Example/Source/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import AVFoundation
+import AVKit
 
-    override func viewDidLoad() {
+import RxSwift
+
+class ViewController: UIViewController
+{
+    var     bag = DisposeBag()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //        "https://tungsten.aaplimg.com/VOD/bipbop_adv_example_v2/master.m3u8"
     }
-
+    
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        let player = AVPlayer(playerItem: nil)
+        
+        let playerController = MyPlayerController()
+        playerController.player = player
+        
+        present(playerController, animated: true, completion: {[weak playerController] in
+            
+            playerController?.playerInterface?.play()
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
